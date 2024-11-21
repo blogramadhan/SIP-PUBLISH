e36fed857685eb382f8f848d295da6e090dc1c24
Kurnia Ramadhan <kramadhan@gmail.com>
2024-11-21 22:21:40 +0700

    update code

--- pages/6_🚀_P3DN.py
+++ pages/6_🚀_P3DN.py
@@ -118,7 +118,7 @@ with menu_p3dn_1:
             baca_realisasi_filter = baca_realisasi[["cobe", "total_realisasi"]].drop_duplicates(subset=["cobe"])
 
             df_proporsi = pd.merge(proporsi, baca_realisasi_filter, left_on="sub_kegiatan_akun", right_on="cobe", how="left")
-            df_proporsi_ok = con.execute(f"SELECT sub_kegiatan_akun, anggaran_belanja, total realisasi, total_realisasi / NULLIF(anggaran_belanja, 0) AS proporsi FROM df_proporsi").df()
+            df_proporsi_ok = con.execute(f"SELECT sub_kegiatan_akun, anggaran_belanja, total_realisasi, total_realisasi / NULLIF(anggaran_belanja, 0) AS proporsi FROM df_proporsi").df()
 
             # df_SIKAPTender_OK_filter_final = df_SIKAPTender_OK_filter.assign(KETERANGAN = np.where(df_SIKAPTender_OK_filter['SKOR_PENILAIAN'] >= 3, "SANGAT BAIK", np.where(df_SIKAPTender_OK_filter['SKOR_PENILAIAN'] >= 2, "BAIK", np.where(df_SIKAPTender_OK_filter['SKOR_PENILAIAN'] >= 1, "CUKUP", "BURUK"))))
 
